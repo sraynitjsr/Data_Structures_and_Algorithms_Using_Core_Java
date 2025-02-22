71ac8572f875e4ef6dcd4b3ae45229b2cd51585e
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-02-22 18:14:40 +0530

    Staring With Core Java For Freshers

--- src/main/java/com/sraynitjsr/MySpringBootApplication.java
+++ src/main/java/com/sraynitjsr/MySpringBootApplication.java
@@ -1,13 +1,15 @@
 package com.sraynitjsr;
 
+import com.sraynitjsr.corejava.MyMain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsaAndCoreJavaApplication {
-
+public class MySpringBootApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DsaAndCoreJavaApplication.class, args);
-	}
+		SpringApplication.run(MySpringBootApplication.class, args);
 
+		System.out.println();
+		MyMain.start();
+	}
 }
---
+++ src/main/java/com/sraynitjsr/corejava/MyMain.java
@@ -0,0 +1,7 @@
+package com.sraynitjsr.corejava;
+
+public class MyMain {
+    public static void start() {
+        System.out.println("Staring With Core Java");
+    }
+}
