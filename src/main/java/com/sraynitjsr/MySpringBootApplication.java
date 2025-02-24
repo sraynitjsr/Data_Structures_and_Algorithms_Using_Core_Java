521a4463e8ee54791a902d5898850d6ca9470f47
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-02-24 19:57:02 +0530

    Thread Sleep and Join

--- src/main/java/com/sraynitjsr/MySpringBootApplication.java
+++ src/main/java/com/sraynitjsr/MySpringBootApplication.java
@@ -1,33 +1,34 @@
 package com.sraynitjsr;
 
-import  com.sraynitjsr._1_datatypes.*;
-import  com.sraynitjsr._2_variables.*;
-import  com.sraynitjsr._3_userdefineddatatypes.*;
-import  com.sraynitjsr._4_controlstatements.*;
-import  com.sraynitjsr._5_loops.*;
-import  com.sraynitjsr._6_array.*;
-import  com.sraynitjsr._7_strings.*;
-import  com.sraynitjsr._8_inputoutput.*;
-import  com.sraynitjsr._9_classinterface.*;
-import  com.sraynitjsr._10_oops.*;
-import  com.sraynitjsr._11_keywords.*;
-import  com.sraynitjsr._12_exceptions.*;
-import  com.sraynitjsr._13_multithreading.*;
-import  com.sraynitjsr._14_collections.*;
-import  com.sraynitjsr._15_java8_and_streams.*;
-import  com.sraynitjsr._16_designpatterns.*;
-import  com.sraynitjsr._17_dsa_practice.*;
+import com.sraynitjsr._1_datatypes.*;
+import com.sraynitjsr._2_variables.*;
+import com.sraynitjsr._3_userdefineddatatypes.*;
+import com.sraynitjsr._4_controlstatements.*;
+import com.sraynitjsr._5_loops.*;
+import com.sraynitjsr._6_array.*;
+import com.sraynitjsr._7_strings.*;
+import com.sraynitjsr._8_inputoutput.*;
+import com.sraynitjsr._9_classinterface.*;
+import com.sraynitjsr._10_oops.*;
+import com.sraynitjsr._11_keywords.*;
+import com.sraynitjsr._12_exceptions.*;
+import com.sraynitjsr._13_multithreading.*;
+import com.sraynitjsr._14_collections.*;
+import com.sraynitjsr._15_java8_and_streams.*;
+import com.sraynitjsr._16_designpatterns.*;
+import com.sraynitjsr._17_dsa_practice.*;
 
-import  org.springframework.boot.SpringApplication;
-import  org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySpringBootApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(MySpringBootApplication.class, args);
         callRespectiveModules();
     }
-    public static void callRespectiveModules() {
+
+    public static void callRespectiveModules() throws Exception {
 /*      MyVariables.start();
         MyDataTypes.start();
         MyUserDefinedDataTypes.start();
@@ -40,11 +41,7 @@ public class MySpringBootApplication {
         MyOOPS.start();
         MyKeywords.start();
         MyExceptionHandling.start();*/
-        try {
-            MyMultithreading.start();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        MyMultithreading.start();
 /*      MyCollections.start();
         MyStreamAPI.start();
         MyDesignPatterns.start();
